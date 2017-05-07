d8bfc6da27139c0598c6c2eae99cfe245e1a3330
OleksiyNosov <oleksiynosov@gmail.com>
2017-05-07 21:27:50 +0300

    Add working cart

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -11,12 +11,6 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     }
   end
 
-  test "can't delete product in cart" do
-    assert_difference('Product.count', 0) do
-      delete products_url(products(:two))
-    end
-  end
-
   test "should get index" do
     get products_url
     assert_response :success
@@ -48,6 +42,14 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   test "should update product" do
     patch product_url(@product), params: { product: @update }
     assert_redirected_to product_url(@product)
+  end
+
+  test "can't delete product in cart" do
+    assert_difference('Product.count', 0) do
+      delete product_url(products(:two))
+    end
+
+    assert_redirected_to products_url
   end
 
   test "should destroy product" do
