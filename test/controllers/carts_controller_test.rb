bc18ab5d92c5648d8654295b1c1a12fb640a2b5f
OleksiyNosov <oleksiynosov@gmail.com>
2017-05-09 01:10:41 +0300

    Add tests and fixtures

--- test/controllers/carts_controller_test.rb
+++ test/controllers/carts_controller_test.rb
@@ -39,6 +39,9 @@ class CartsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should destroy cart" do
+    post line_items_url, params: { product_id: products(:ruby_book).id }
+    @cart = Cart.find(session[:cart_id])
+
     assert_difference('Cart.count', -1) do
       delete cart_url(@cart)
     end
