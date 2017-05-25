135a2051fc802069264a1db38e04ea1ccec9a26a
OleksiyNosov <oleksiynosov@gmail.com>
2017-05-26 01:43:03 +0300

    Controllers authorize fix

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,15 +1,5 @@
 class AdminController < ApplicationController
-  before_action :authorize
-
   def index
     @total_oders = Order.count
   end
-
-  protected
-
-    def authorize
-      unless User.find_by(id: session[:user_id])
-        redirect_to login_url, notice: "Please log in"
-      end
-    end
 end
